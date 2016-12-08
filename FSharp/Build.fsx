7484febf23f0d88487b6f5ac20116f4486f66d49
Mark Seemann <mark@ploeh.dk>
2016-12-08 14:36:05 +0100

    Added build script

---
+++ FSharp/Build.fsx
@@ -0,0 +1,12 @@
+﻿#r @"packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+Target "Build" <| fun _ ->
+    !! "**/BookingApi.sln"
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+
+"Build"
+
+RunTargetOrDefault "Build"
---
+++ FSharp/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+packages/FAKE/tools/FAKE.exe
