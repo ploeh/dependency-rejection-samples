8c4f9ccdd736131a03c39189daabffe913653db0
Mark Seemann <mark@ploeh.dk>
2016-12-12 15:01:11 +0100

    Shortened code

--- FSharp/BookingApi/MaîtreD.fs
+++ FSharp/BookingApi/MaîtreD.fs
@@ -10,8 +10,7 @@ type Reservation = {
 
 // int -> Reservation list -> Reservation -> Reservation option
 let tryAccept capacity reservations reservation =
-    let reservedSeats =
-        reservations |> List.sumBy (fun x -> x.Quantity)
+    let reservedSeats = reservations |> List.sumBy (fun x -> x.Quantity)
     if reservedSeats + reservation.Quantity <= capacity
     then { reservation with IsAccepted = true } |> Some
     else None
