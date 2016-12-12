4c656f6f61a01266713647725b89bd16017b05f2
Mark Seemann <mark@ploeh.dk>
2016-12-12 14:06:01 +0100

    Refactored composition to use Option.map

--- FSharp/BookingApi/Composition.fs
+++ FSharp/BookingApi/Composition.fs
@@ -12,6 +12,5 @@ let connectionString = ""
 
 // Reservation -> int option
 let tryAcceptComposition reservation =
-    match tryAccept 10 (DB.readReservations connectionString) reservation with
-    | None -> None
-    | Some r -> (DB.createReservation connectionString) r |> Some
+    tryAccept 10 (DB.readReservations connectionString) reservation
+    |> Option.map (fun r -> DB.createReservation connectionString r)
