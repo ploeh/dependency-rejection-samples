1dcf2d5a88718c4a97d2988a0ad868a52f5052a5
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2017-02-08 11:34:59 +0200

    Ported 'tryAccept behaves correctly when it can accept' from F#.
    Original version:
    https://github.com/ploeh/dependency-rejection-samples/blob/22230733a36f0a425deaafbee14a8922f6df83e6/FSharp/BookingApi/Ma%C3%AEtreDTests.fs#L17-L31

--- Haskell/MaîtreD.hs
+++ Haskell/MaîtreD.hs
@@ -1,10 +1,18 @@
 module MaîtreD where
 
-import Data.Time (ZonedTime)
+import Data.Time (ZonedTime (zonedTimeToLocalTime, zonedTimeZone))
 
 data Reservation =
   Reservation { date :: ZonedTime, quantity :: Int, isAccepted :: Bool }
   deriving (Show)
+
+instance Eq Reservation where
+  x == y =
+       zonedTimeZone        (date x) == zonedTimeZone        (date y)
+    && zonedTimeToLocalTime (date x) == zonedTimeToLocalTime (date y)
+    && quantity                   x  ==  quantity                  y
+    && isAccepted                 x  == isAccepted                 y
+
 
 tryAccept :: Int -> [Reservation] -> Reservation -> Maybe Reservation
 tryAccept capacity reservations reservation =
--- Haskell/MaîtreDTests.hs
+++ Haskell/MaîtreDTests.hs
@@ -1,1 +1,45 @@
 module MaîtreDTests where
+
+import           Control.Monad      (liftM2)
+import           Data.Time          (ZonedTime (..), defaultTimeLocale,
+                                     iso8601DateFormat, parseTimeOrError)
+import           Data.Time.Calendar (gregorianMonthLength)
+import           MaîtreD
+import           Test.QuickCheck
+import           Text.Printf        (printf)
+
+instance Arbitrary ZonedTime where
+  arbitrary = do
+    y <- choose (1, 9999)
+    m <- choose (1, 12)
+    d <- choose (1, gregorianMonthLength y m)
+    return $
+      (parseTimeOrError True defaultTimeLocale $ iso8601DateFormat Nothing)
+      (printf "%04d" y ++ "-" ++ printf "%02d" m ++ "-" ++ printf "%02d" d)
+
+genReservation :: Gen Reservation
+genReservation = do
+  bookingDate <- arbitrary
+  Positive qt <- arbitrary
+  trueOrFalse <- arbitrary
+  return Reservation
+    { date       = bookingDate
+    , quantity   = qt
+    , isAccepted = trueOrFalse }
+
+sumBy :: Num a => (b -> a) -> [b] -> a
+sumBy x xs = sum $ map x xs
+
+tryAcceptBehavesCorrectlyWhenItCanAccept :: NonNegative Int -> Property
+tryAcceptBehavesCorrectlyWhenItCanAccept (NonNegative excessCapacity) =
+  forAll
+    (liftM2 (,) genReservation $ listOf genReservation)
+    (\(reservation, reservations) ->
+      let capacity =
+            excessCapacity
+            + sumBy quantity reservations
+            + quantity reservation
+
+          actual = tryAccept capacity reservations reservation
+
+      in Just (reservation { isAccepted = True }) == actual)
